e2311692d00c6633f268ce5be8e27a86a97e1e42
WarnerYang <934797303@qq.com>
2019-02-17 23:25:25 +0800

    fixed issuse#7

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
             </div>
             <ul class="post-meta">
 				<!-- <li itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e('作者: '); ?><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li> -->
-				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y -m-d'); ?></time></li>
+				<li><?php _e('时间: '); ?><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></li>
 				<?php if(isset($this->options->plugins['activated']['Views'])) _e('<li>').Views_Plugin::theViews(). _e('</li>'); ?>
 				<li itemprop="interactionCount"><a itemprop="discussionUrl" href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('评论', '1 条评论', '%d 条评论'); ?></a></li>
 				<li><a itemtype="url" href="<?php $this->permalink() ?>">阅读全文</a></li>
